e999e15a9c3b82fdf105f2d5999f5e94bb3baf1f
Khoi <hienance@gmail.com>
2025-10-21 14:02:08 +0700

    Search filters

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -3,30 +3,18 @@ import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
 import { Checkbox } from "@/components/ui/checkbox";
+
+
 Progress
 
 export default function Home() {
+ 
+
   return (
       <div className="p-4">
       <div className="flex flex-col gap-y-4">
-        <div>
-          <Button variant="elevated">
-                      this is not a button
-          </Button>
-        </div>
-        <div>
-          <Input placeholder="I am an input"/> 
-        </div>
-        <div>
-          <Progress value={50}/>
-        </div>
-        <div>
-          <Textarea placeholder={"I am a text area"}/>
-        </div>
-        <div>
-          <Checkbox/>
-        </div>
       </div>
+      Home
       </div>
   );
 }
