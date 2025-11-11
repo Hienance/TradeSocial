7c5664f76f2a9c9e1312a68059592c8edc01eb90
Khoi <hienance@gmail.com>
2025-11-11 10:46:13 +0700

    API Filter Sorting

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,9 +2,11 @@ import { authRouter } from '@/modules/auth/server/procedure';
 import { createTRPCRouter } from '../init'; 
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 import { productsRouter } from '@/modules/products/server/procedures';
+import { tagsRouter } from '@/modules/tags/server/procedures';
 
 
 export const appRouter = createTRPCRouter({
+    tags: tagsRouter,
     auth: authRouter,
     categories : categoriesRouter,
     products: productsRouter
